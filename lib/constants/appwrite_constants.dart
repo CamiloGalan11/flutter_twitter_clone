32efbfc8ea3e1026dfc4e0346fe42115451f2f51
Camilo Galan <cagalano@ufpso.edu.co>
2023-05-31 18:15:40 -0500

    segundo parcial moviles

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,13 +1,13 @@
 class AppwriteConstants {
-  static const String databaseId = '63ca9fd892b6a9f5e135';
-  static const String projectId = '63ca9d4f81ce9074804f';
-  static const String endPoint = 'http://YOUR_IP/v1';
+  static const String databaseId = '6475942aeb621dd33d01';
+  static const String projectId = '64758cc1db8d0a12eca6';
+  static const String endPoint = 'http://192.168.1.69/v1';
 
-  static const String usersCollection = '63cb8ae73960973b0635';
-  static const String tweetsCollection = '63cbd6781a8ce89dcb95';
-  static const String notificationsCollection = '63cd5ff88b08e40a11bc';
+  static const String usersCollection = '6476d1cb8d8838120b07';
+  static const String tweetsCollection = '6476d7bd3704a5d5a486';
+  static const String notificationsCollection = '6477bd343749421a6532';
 
-  static const String imagesBucket = '63cbdab48cdbccb6b34e';
+  static const String imagesBucket = '6477aa34261b8dcca005';
 
   static String imageUrl(String imageId) =>
       '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
